03dd6388f3225c8f025f239ef396ad59c78ed2f4
robshape <robshape@users.noreply.github.com>
2018-02-18 00:51:22 +0100

    Added Truffle Framework

---
+++ ethereum/truffle.js
@@ -0,0 +1,10 @@
+module.exports = {
+  contracts_build_directory: '../dist/ethereum',
+  networks: {
+    development: {
+      host: '127.0.0.1',
+      network_id: '*',
+      port: 9545,
+    },
+  },
+};
