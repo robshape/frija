f06d8eba2a40130eeade50ee68e5250b21e2050e
robshape <robshape@users.noreply.github.com>
2019-01-01 21:37:46 +0100

    update copyright years for new files

--- src/server/app/index.js
+++ src/server/app/index.js
@@ -1,7 +1,7 @@
 /*
 
   Frija - The Swedish general election and Riksdag on the Ethereum blockchain.
-  Copyright (C) 2018 Frija contributors.
+  Copyright (C) 2019 Frija contributors.
 
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
