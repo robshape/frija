3dd3e92a622d5882cec80a53b21c5b43e788e533
robshape <robshape@users.noreply.github.com>
2019-08-21 18:06:39 +0200

    update string

--- scripts/setup-project.sh
+++ scripts/setup-project.sh
@@ -17,6 +17,6 @@ echo "[*] INSTALLING DEVELOPMENT DEPENDENCIES"
 npm ci
 echo
 
-echo "[*] GENERATING CERTIFICATE"
+echo "[*] CREATING CERTIFICATE"
 openssl req -keyout server.key -new -nodes -out server.crt -subj '/CN=localhost' -x509
 echo
