3bef0081aa7de5c4f8e90e422924d10d6daa0f36
robshape <robshape@users.noreply.github.com>
2019-09-19 16:51:36 +0200

    add GitHub Actions CI workflow

--- scripts/setup-project.sh
+++ scripts/setup-project.sh
@@ -3,13 +3,13 @@ set -euo pipefail
 
 echo "[*] INSTALLING DEPENDENCIES"
 npm ci
-npx lerna bootstrap --hoist
+npm run bootstrap
 echo
 
 echo "[*] CREATING CERTIFICATE"
 openssl req -keyout server.key -new -nodes -out server.crt -subj '/CN=localhost' -x509
 echo
 
-echo "[*] BUILDING CONTRACTS"
+echo "[*] BUILDING ETHEREUM PACKAGE"
 npx lerna run --scope @frija/ethereum build
 echo
